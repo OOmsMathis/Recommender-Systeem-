1d08bd360b72ec74257b43d72763a87bb1519118
Charlesdelhoute <charlesdelhoute@hotmail.com>
2025-05-14 17:28:05 +0200

    rendue 4

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 # local imports
-from models import ModelBaseline1, ModelBaseline2, ModelBaseline3, ModelBaseline4
+from models import ModelBaseline1, ModelBaseline2, ModelBaseline3, ModelBaseline4,ContentBased
 
 
 
@@ -9,7 +9,20 @@ class EvalConfig:
         ("baseline_1", ModelBaseline1, {}),  # model_name, model class, model parameters (dict)
         ("baseline_2", ModelBaseline2, {}),
         ("baseline_3", ModelBaseline3, {}),
-        ("baseline_4", ModelBaseline4, {"random_state": 1})
+        ("baseline_4", ModelBaseline4, {"random_state": 1}),
+         #Ajouts pour le Content-Based
+        ("content_random_score", ContentBased, {
+            "features_method": None,
+            "regressor_method": "random_score"
+        }),
+        ("content_random_sample", ContentBased, {
+            "features_method": None,
+            "regressor_method": "random_sample"
+        }),
+        ("content_linear", ContentBased, {
+            "features_method": "title_length",  
+            "regressor_method": "linear"
+        }),
     ]
     split_metrics = ["mae","rmse"]
     loo_metrics = ["hit_rate"]
--- models.py
+++ models.py
@@ -4,9 +4,16 @@ from collections import defaultdict
 # third parties imports
 import numpy as np
 import random as rd
+import pandas as pd
 from surprise import AlgoBase
 from surprise import KNNWithMeans
 from surprise import SVD
+from sklearn.linear_model import LinearRegression
+from surprise import PredictionImpossible
+from loaders import load_ratings
+from loaders import load_items
+import constants as C
+from constants import Constant as C
 
 
 def get_top_n(predictions, n):
@@ -81,3 +88,113 @@ class ModelBaseline3(AlgoBase):
 class ModelBaseline4(SVD):
     def __init__(self,random_state = 1):
         SVD.__init__(self, n_factors=100)
+
+#content_based
+class ContentBased(AlgoBase):
+    def __init__(self, features_method, regressor_method):
+        AlgoBase.__init__(self)
+        self.features_method = features_method 
+        self.regressor_method = regressor_method
+        self.content_features = self.create_content_features(features_method)
+
+    def create_content_features(self, features_method):
+        """Content Analyzer"""
+        df_items = load_items()
+        df_features = pd.DataFrame(index=df_items.index)
+        if features_method is None:
+           df_features = pd.DataFrame(index=df_items.index)
+        elif features_method == "title_length": # a naive method that creates only 1 feature based on title length
+            df_title_length = df_items[C.LABEL_COL].apply(lambda x: len(x)).to_frame('title_length')
+            df_title_length['title_length'] = df_title_length['title_length'].fillna(0).astype(int)
+            mean_title_length = int(df_title_length['title_length'].replace(0, np.nan).mean())
+            df_title_length.loc[df_title_length['title_length'] == 0, 'title_length'] = mean_title_length
+            # Normaliser la longueur des titre entre 0 et 1
+            title_length_min = df_title_length['title_length'].min()
+            title_length_max = df_title_length['title_length'].max()
+            df_title_length['title_length'] = (df_title_length['title_length'] - title_length_min) / (title_length_max - title_length_min)
+            df_features = pd.concat([df_features, df_title_length], axis=1)
+        elif features_method == "Year_of_release":
+            year = df_items[C.LABEL_COL].str.extract(r'\((\d{4})\)')[0].astype(float)
+            df_year = year.to_frame(name='year_of_release')
+            mean_year = df_year.replace(0, np.nan).mean().iloc[0]
+            df_year['year_of_release'] = df_year['year_of_release'].fillna(mean_year).astype(int)
+             # Normaliser les dates de sortie 
+            year_min = df_year['year_of_release'].min()
+            year_max = df_year['year_of_release'].max()
+            df_year['year_of_release'] = (df_year['year_of_release'] - year_min) / (year_max - year_min)
+            df_features = df_features.join(df_year, how='left')
+        else: # (implement other feature creations here)
+            raise NotImplementedError(f'Feature method {features_method} not yet implemented')
+        return df_features
+    
+
+    def fit(self, trainset):
+        """Profile Learner"""
+        self.content_features = self.create_content_features(self.features_method)
+        AlgoBase.fit(self, trainset)
+        self.user_profile = {u: None for u in trainset.all_users()}
+        for u in self.user_profile:
+            user_items = trainset.ur[u]
+            if len(user_items) > 0:
+                # Sépare les item_ids internes et les notes
+                user_ratings = self.trainset.ur[u]
+                df_user = pd.DataFrame(user_ratings, columns=['inner_item_id', 'user_ratings'])
+                # Conversion des item_id internes (Surprise) en item_id "raw" (MovieLens)
+                df_user["item_id"] = df_user["inner_item_id"].map(self.trainset.to_raw_iid)
+                # Fusion avec les features de contenu (sur l'index = item_id raw)
+                df_user = df_user.merge(self.content_features, how='left', left_on='item_id', right_index=True)
+                # Préparation des features et des cibles pour l'entraînement
+                feature_names = list(self.content_features.columns)
+                X = df_user[feature_names].values
+                y = df_user['user_ratings'].values
+                # Gère les NaNs dans les features
+                X = np.nan_to_num(X)
+
+                if self.regressor_method == 'random_score':
+                    self.user_profile[u] = rd.uniform(0.5, 5.0)
+                elif self.regressor_method == 'random_sample':
+                    self.user_profile[u] = [rating for _, rating in self.trainset.ur[u]]
+                elif self.regressor_method == 'linear':
+                    # Use linear regression
+                    model = LinearRegression(fit_intercept=True)
+                    model.fit(X, y)
+                    self.user_profile[u] = model
+                else:
+                    self.user_profile[u] = None
+                    # implement here the regressor fitting) 
+            else:
+             self.user_profile[u] = None
+             
+        
+    def estimate(self, u, i):
+        """Scoring component used for item filtering"""
+        # First, handle cases for unknown users and items
+        if not (self.trainset.knows_user(u) and self.trainset.knows_item(i)):
+            raise PredictionImpossible('User and/or item is unkown.')
+
+        if self.user_profile[u] is None:
+            return self.trainset.global_mean
+
+        raw_item_id = self.trainset.to_raw_iid(i)
+        if raw_item_id in self.content_features.index:
+            item_features = self.content_features.loc[raw_item_id].values.reshape(1, -1)
+        else:
+            return self.trainset.global_mean
+    
+        if self.regressor_method == 'random_score':
+            rd.seed()
+            score = rd.uniform(0.5,5)
+
+        elif self.regressor_method == 'random_sample':
+            rd.seed()
+            score = rd.choice(self.user_profile[u])
+        elif self.regressor_method == 'linear':
+            score = self.user_profile[u].predict(item_features)[0]
+
+        else:
+            score=None
+            
+
+        return score
+
+
