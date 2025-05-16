fbd3f74d28288a4b45b11b00e9981031c720c639
Charlesdelhoute <charlesdelhoute@hotmail.com>
2025-05-16 09:56:42 +0200

    corr

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 class Constant:
 
-    DATA_PATH = Path('/Users/delhoutecharles/Documents/GitHub/Recommender-Systeem-/data/test')  # -- fill here the dataset size to use
+    DATA_PATH = Path('/Users/delhoutecharles/Documents/GitHub/Recommender-Systeem-/data/small')  # -- fill here the dataset size to use
 
     # Content
     CONTENT_PATH = DATA_PATH / 'content'
